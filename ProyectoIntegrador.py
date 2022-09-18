e80b6fcdc3dce2e50cadd052ee5468144633d740
AlcidesSainz <alcidessainz99@gmail.com>
2022-09-18 16:28:46 -0500

    almost finished

--- ProyectoIntegrador.py
+++ ProyectoIntegrador.py
@@ -5,6 +5,7 @@ from tkinter import messagebox
 
 
 def menuPrincipal():
+    # Creando ventana del menu principa;
     global ventana
     ventana = Tk()
     ventana.geometry("500x400+600+100")
@@ -12,19 +13,66 @@ def menuPrincipal():
     ventana.title("Menu Principal")
     ventana.iconbitmap("logo.ico")
 
+    # Agregando imagen del instituto
     img = PhotoImage(file="logoImagen.png")
     Label(ventana, image=img).pack()
-
-    Label(text="Acceso al Sistema", bg="navy", fg="white", width="300", height="3", font=("Calibri", 15)).pack()
+    # Creando texto de Acceso al Sistema
+    Label(text="Acceso al Sistema", bg="#067b7a", fg="white",
+          width="300", height="3", font=("Calibri", 15)).pack()
     Label(text="").pack()
-    Button(text="Iniciar Sesion", height="3", width="30", command=iniciarSesion, cursor="hand2").place(x=20,y=250)
+    # Boton Iniciar Sesion
+    Button(text="Iniciar Sesion", height="3", width="30",
+           command=iniciarSesion, cursor="hand2").place(x=20, y=250)
     Label(text="").pack()
-    Button(text="Registrarse", height="3", width="30", command=registrar, cursor="hand2").place(x=260,y=250)
+    # Boton para registrarse
+    Button(text="Registrarse", height="3", width="30",
+           command=registrar, cursor="hand2").place(x=260, y=250)
     Label(text="").pack()
+    # Boton para salir de la app
     Button(text="Salir", height="3", width="30", cursor="hand2",
-           command=sys.exit).place(x=130,y=320)
+           command=sys.exit).place(x=130, y=320)
 
     ventana.mainloop()
+
+
+def ventanaCronometroFuncion():
+    global proceso
+    proceso = 'after#0'
+    ventanaCronometro = Toplevel(ventana1)
+    ventanaCronometro.title("Cronometro")
+    ventanaCronometro.geometry('340x200')
+    ventanaCronometro.iconbitmap('crono.ico')
+    ventanaCronometro.configure(background='silver')
+    global cronometro
+    global nombreUsuarioEntradaCronometro
+    nombreUsuarioEntradaCronometro = nombreUsuarioEntrada
+    # Creando Label Usuario
+    Label(ventanaCronometro, text="Usuario: ", bg="silver").place(x=20, y=0)
+    # Creando Label con el nombre de usuario
+    Label(ventanaCronometro, text=nombreUsuarioEntradaCronometro.get(),
+          bg="silver").place(x=69, y=0)
+    cronometro = Label(ventanaCronometro, fg='green',
+                       width=7, font=('verdana', 50), bg='black')
+    cronometro.place(x=20, y=20)
+    # Boton Iniciar
+    btn_inicio = Button(ventanaCronometro, font=(
+        'courier', 10, 'bold'), text='Iniciar', command=iniciar)
+    btn_inicio.place(x=20, y=120)
+    # Boton Detener
+    btn_detener = Button(ventanaCronometro, font=(
+        'courier', 10, 'bold'), text='Detener', command=detener)
+    btn_detener.place(x=140, y=120)
+    # Boton Guardar
+    btn_Guardar = Button(ventanaCronometro, font=(
+        'courier', 10, 'bold'), text='Guardar', command=guardar)
+    btn_Guardar.place(x=252, y=120)
+    # Boton Ver Tiempo Guardado
+    Button(ventanaCronometro, font=('courier', 10, 'bold'),
+           text='Ver Resultado',command=verTiempo).place(x=10, y=160)
+    # Boton Salir
+    btn_Salir = Button(ventanaCronometro, font=(
+        'courier', 10, 'bold'), text='Salir', command=salir)
+    btn_Salir.place(x=146, y=160)
 
 
 def iniciarSesion():
@@ -35,7 +83,7 @@ def iniciarSesion():
     ventana1.title("Inicio de sesion")
     ventana1.iconbitmap("logo.ico")
 
-    Label(ventana1, text="Ingrese su Usuario y Contraseña", bg="navy", fg="white", width="300", height="3",
+    Label(ventana1, text="Ingrese su Usuario y Contraseña", bg="#067b7a", fg="white", width="300", height="3",
           font=("Calibri", 15)).pack()
     Label(ventana1, text="").pack
 
@@ -54,13 +102,16 @@ def iniciarSesion():
     Label(ventana1).pack()
 
     Label(ventana1, text="Contraseña: ").pack()
-    constrasenaUsuarioEntrada = Entry(ventana1, textvariable=constrasenaUsuarioVerificar, show="*")
+    constrasenaUsuarioEntrada = Entry(
+        ventana1, textvariable=constrasenaUsuarioVerificar, show="*")
     constrasenaUsuarioEntrada.pack()
     Label(ventana1).pack
     Label(ventana1, text="").pack()
 
-    Button(ventana1, text="Iniciar Sesion", cursor="hand2", command=verificarUsuario).pack()
-    
+    Button(ventana1, text="Iniciar Sesion",
+           cursor="hand2", command=verificarUsuario).pack()
+
+
 def registrar():
     global ventana2
     ventana2 = Toplevel(ventana)
@@ -75,7 +126,7 @@ def registrar():
     contrasenaEntrada = StringVar()
 
     Label(ventana2, text="Por Favor Ingrese los datos que se solicitan \n"
-                         "Para Realizar el Registro", bg="navy", fg="white", width="300", height="3",
+                         "Para Realizar el Registro", bg="#067b7a", fg="white", width="300", height="3",
           font=("Calibri", 15)).pack()
     Label(ventana2, text="").pack()
 
@@ -89,7 +140,8 @@ def registrar():
     contrasenaEntrada.pack()
     Label(ventana2).pack()
 
-    Button(ventana2, text="Registrar", cursor="hand2", command=verificarNombreUsuario).pack()
+    Button(ventana2, text="Registrar", cursor="hand2",
+           command=verificarNombreUsuario).pack()
 
 
 def insertarUsuarios():
@@ -98,7 +150,8 @@ def insertarUsuarios():
     sqliteQuery = "INSERT INTO usuarios (userName_usuario,contrasena) VALUES ('{0}','{1}')".format(
         nombreUsuarioEntrada.get(), contrasenaEntrada.get())
     if nombreUsuarioEntrada.get() == "" or contrasenaEntrada.get() == "":
-        messagebox.showerror(message="No se puede dejar en blanco ningun campo", title="ERROR", )
+        messagebox.showerror(
+            message="No se puede dejar en blanco ningun campo", title="ERROR", )
         ventana2.destroy()
     else:
         try:
@@ -116,7 +169,8 @@ def verificarNombreUsuario():
     conexion = sqlite3.connect("Base de Datos/proyecto_db.db")
     cursor = conexion.cursor()
 
-    cursor.execute("SELECT userName_usuario FROM usuarios WHERE userName_usuario='" + nombreUsuarioEntrada.get() + "'")
+    cursor.execute("SELECT userName_usuario FROM usuarios WHERE userName_usuario='" +
+                   nombreUsuarioEntrada.get() + "'")
     if cursor.fetchall():
         messagebox.showerror(message="El usuario ya existe", title="ERROR")
         ventana2.destroy()
@@ -132,13 +186,87 @@ def verificarUsuario():
         "SELECT contrasena FROM usuarios WHERE userName_usuario='" + nombreUsuarioVerificar.get() + "'and contrasena='" + constrasenaUsuarioVerificar.get() + "'")
 
     if cursor.fetchall():
-        ventanaCronometro()
-        ventana.withdraw()
-        ventana1.withdraw()
+        ventanaCronometroFuncion()
+
     else:
-        messagebox.showinfo(title="Inicio Sesion", message="Usuario y/o Contraseña Incorrecta ")
+        messagebox.showinfo(title="Inicio Sesion",
+                            message="Usuario y/o Contraseña Incorrecta ")
         ventana1.destroy()
     conexion.close()
+
+
+def iniciar(hora=0, minuto=0, segundo=0):
+    global proceso
+    global guardar_minuto
+    global guardar_segundo
+    global cronometro
+    guardar_minuto = minuto
+    guardar_segundo = segundo
+    cronometro.after_cancel(proceso)
+    if segundo > 59:
+        segundo = 0
+        minuto += 1
+        if minuto > 59:
+            minuto = 0
+            hora += 1
+            if hora > 23:
+                hora = 0
+    cronometro.after_cancel(proceso)
+    cronometro['text'] = str(hora)+':'+str(minuto)+':' + str(segundo)
+    proceso = cronometro.after(1090, iniciar, (hora), (minuto), (segundo+1))
+
+
+def detener():
+    try:
+        cronometro.after_cancel(proceso)
+
+    except ValueError:
+        pass
+
+
+def guardar():
+    conexion = sqlite3.connect("Base de Datos/proyecto_db.db")
+    cursor = conexion.cursor()
+    try:
+       # cronometro.after_cancel(proceso)
+
+        sqlQuery = "UPDATE usuarios SET segundos= {0} , minutos = {1} WHERE userName_usuario='{2}'".format(
+            guardar_segundo, guardar_minuto, nombreUsuarioEntradaCronometro.get())
+
+        cursor.execute(sqlQuery)
+        conexion.commit()
+        messagebox.showinfo(
+            message="Tiempo Registrado con exito", title="Aviso")
+    except Exception as e:
+        messagebox.showerror(
+            message="Error al guardar el tiempo. {0}".format(e), title='Error')
+
+
+def verTiempo():
+    conexion = sqlite3.connect("Base de Datos/proyecto_db.db")
+    cursor = conexion.cursor()
+
+    try:
+        sqlQuery = "SELECT minutos, segundos FROM usuarios WHERE userName_usuario= '{0}'".format(
+            nombreUsuarioEntrada.get())
+
+        cursor.execute(sqlQuery)
+
+        record=cursor.fetchone()
+
+        messagebox.showinfo(
+            message="{0} minutos con {1} segundos".format(record[0], record[1]), title='Tiempo')
+
+       
+
+    except Exception as e:
+        messagebox.showerror(
+            message="Error al guardar el tiempo. {0}".format(e), title='Error')
+
+
+def salir():
+    quit()
+
 
 if __name__ == "__main__":
     menuPrincipal()
